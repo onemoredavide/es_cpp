56659f8ef7cd6811f8e8b4633a64f4bfb744e0a8
onemoredavide <davideantipa@gmail.com>
2022-01-13 08:22:36 +0100

    correzione cicli3_es8.cpp

--- cicli3/cicli3_es8.cpp
+++ cicli3/cicli3_es8.cpp
@@ -5,13 +5,12 @@ using namespace std;
 int main(){
     /*Inserire una serie di numeri e sommarli solo se sono dispari. Interrompere l'immissione quando la somma
     raggiunge o supera il valore 100. (numerosità non definita)*/
-    int d=0,val, s=0;
+    int val, s=0;
     do{
         cin>>val;
         if(!val%2==0){
-            d++;
             s+=val;
         }
     } while(s<100);
-    cout<<s<<" | "<<d<<endl;
+    cout<<"Somma: "<<s<<endl;
 }
