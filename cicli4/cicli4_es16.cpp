91d5122d6d00317a6b545949abbd3fe64a2a8a7f
onemoredavide <87283438+onemoredavide@users.noreply.github.com>
2022-02-01 10:44:53 +0100

    Update cicli4_es16.cpp

--- cicli4/cicli4_es16.cpp
+++ cicli4/cicli4_es16.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
 int main(){
 	int val, c1=0, c2=0;
 	do {
-		cin>>val;
+		do {
+			cin>>val;
+			if(val<0||val>2)
+				cout<<"Sbagliato"<<endl;	
+		} while (val<0||val>2);
 		if (val==0) break;
 		(val==1) ? c1++ : c2++;
-	} while (val!=0 && val==1 || val==2);
+	} while (val!=0);
 	return 0;
 }
-
