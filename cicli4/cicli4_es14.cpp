5757fa9fb1079d707f0283f72caca918cbe2809b
onemoredavide <davideantipa@gmail.com>
2022-02-02 15:53:40 +0100

    changes in cicli4_es14.cpp

--- cicli4/cicli4_es14.cpp
+++ cicli4/cicli4_es14.cpp
@@ -6,6 +6,11 @@ int main ()
 {
     /*Leggere in input dei numeri compresi tra 50 e 100(effettuare il controllo) finchè la loro somma diviene
     maggiore di 500. Visualizzare il numero di valori immessi dall’utente*/
+
+    /*
+
+    // CICLO DO-WHILE
+
     int s=0,val,c;
     do {
         do 
@@ -17,5 +22,30 @@ int main ()
         c++;
     } while (s<=500);
     cout<<"Valori inseriti: "<<c<<endl;
+    */
+
+   // VERSIONE CON IL CICLO WHILE
+
+    int val,s=0,i=0;
+    do
+    {
+        cin>>val;
+    } while (val<50||val>100);
+    s+=val;
+    i++;
+
+    while (s<=500)
+    {
+        if (s>500) 
+            break;
+        do
+        {
+            cin>>val;
+        } while (val<50||val>100);
+        s+=val;
+        cout<<"SOMMA = "<<s<<endl;
+        i++;
+    }
+    cout<<"Numri inseriti = "<<i;
     return 0;
 }
