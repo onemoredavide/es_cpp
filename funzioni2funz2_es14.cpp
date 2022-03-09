817fc3a57f920af8772af50e395f4a828d92262d
onemoredavide <87283438+onemoredavide@users.noreply.github.com>
2022-03-09 10:28:08 +0100

    Create funzioni2funz2_es14.cpp

---
+++ funzioni2funz2_es14.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int input(){
+	cout<<"Inserisci un numero: "<<endl;
+	 int x;
+	 cin>>x;
+	 return x;
+}
+
+int quadrato(int x) {
+	int i=0,q=1,s=0;
+	while(i<x){
+		s+=q;
+		q+=2;
+		i++;
+	}
+	return s;
+}
+
+int main() {
+	
+	/*
+		i quadrato di n si puo' calcolare sommando i primi n numeri dispari
+		dato n calcolare il quadrato (funzione input e quadrato)
+	*/
+	
+	int x;
+	x=input();
+	
+	cout<<"Il quadrato e' "<<quadrato(x);
+
+	
+	return 0;
+}
+
+
+
